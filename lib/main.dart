c6566524101d9235ae4dd06b1b328464c979e910
Takumi Okayasu <takumi_okys@icloud.com>
2025-04-28 09:26:49 +0900

    feat: darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ankipan',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: const HomeScreen(),
       debugShowCheckedModeBanner: true,
     );
